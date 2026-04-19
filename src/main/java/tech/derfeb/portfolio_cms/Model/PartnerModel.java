b1f317ab3d60f04ba244756f427efbdcf36ea2ae
Der-Feb <bugingoderfeb@gmail.com>
2026-04-19 15:10:26 +0200

    feat: make PartnerModel public

--- src/main/java/tech/derfeb/portfolio_cms/Model/PartnerModel.java
+++ src/main/java/tech/derfeb/portfolio_cms/Model/PartnerModel.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-class PartnerModel {
+public class PartnerModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
