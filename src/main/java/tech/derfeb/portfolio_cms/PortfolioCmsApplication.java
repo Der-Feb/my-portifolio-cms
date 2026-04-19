f04a0eb5b4475c0f9d9f7675834d52e34d7259f8
Der-Feb <bugingoderfeb@gmail.com>
2026-04-19 11:18:05 +0200

    configuring env in the project

--- src/main/java/tech/derfeb/portfolio_cms/PortfolioCmsApplication.java
+++ src/main/java/tech/derfeb/portfolio_cms/PortfolioCmsApplication.java
@@ -3,10 +3,16 @@ package tech.derfeb.portfolio_cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PortfolioCmsApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables into System properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(PortfolioCmsApplication.class, args);
 	}
 
