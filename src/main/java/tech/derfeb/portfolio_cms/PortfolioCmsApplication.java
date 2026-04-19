c9c6452e895264bd8aa4fe7beb2a267e79753091
Der-Feb <bugingoderfeb@gmail.com>
2026-04-19 11:06:19 +0200

    first commit

---
+++ src/main/java/tech/derfeb/portfolio_cms/PortfolioCmsApplication.java
@@ -0,0 +1,13 @@
+package tech.derfeb.portfolio_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/derfeb/portfolio_cms/PortfolioCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.derfeb.portfolio_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
