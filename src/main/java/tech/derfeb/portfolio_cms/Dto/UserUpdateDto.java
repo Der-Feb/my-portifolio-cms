ec50ff200cbea44a269c38f55cfb7e5a60b7351a
Der-Feb <bugingoderfeb@gmail.com>
2026-04-23 11:54:15 +0200

    feat: add validation annotations to UserUpdateDto

--- src/main/java/tech/derfeb/portfolio_cms/Dto/UserUpdateDto.java
+++ src/main/java/tech/derfeb/portfolio_cms/Dto/UserUpdateDto.java
@@ -1,11 +1,16 @@
 package tech.derfeb.portfolio_cms.Dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.Set;
 
 @Data
 public class UserUpdateDto {
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
     private Set<String> roles;
 }
