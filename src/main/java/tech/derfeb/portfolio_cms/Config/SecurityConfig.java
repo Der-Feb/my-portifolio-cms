7839f6da40823c3f674402ad9d13c965eea3d33a
Der-Feb <bugingoderfeb@gmail.com>
2026-04-23 15:46:48 +0200

    chore: update CORS configuration for production deployment

--- src/main/java/tech/derfeb/portfolio_cms/Config/SecurityConfig.java
+++ src/main/java/tech/derfeb/portfolio_cms/Config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:2030")); // Frontend URL
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://*.bugingo-eric.vercel.app", "http://localhost:2030"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
