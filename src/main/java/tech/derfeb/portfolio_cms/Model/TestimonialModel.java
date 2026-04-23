ac5d1ba526b35b81df8d417cd2d47eb6e81ec8e6
Der-Feb <bugingoderfeb@gmail.com>
2026-04-23 10:27:38 +0200

    feat: add TestimonialModel entity

---
+++ src/main/java/tech/derfeb/portfolio_cms/Model/TestimonialModel.java
@@ -0,0 +1,54 @@
+package tech.derfeb.portfolio_cms.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "testimonials")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestimonialModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String role;
+    private String company;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    private String projectWorkedOn;
+    private String profileImage;
+
+    @ElementCollection
+    @CollectionTable(name = "testimonial_contact_links", joinColumns = @JoinColumn(name = "testimonial_id"))
+    private List<ContactLink> contactLinks = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ContactLink {
+        private String label;
+        private String url;
+    }
+}
