1dc6de81e2aa9d0db4b476728e2aa6f9d93e8e92
Der-Feb <bugingoderfeb@gmail.com>
2026-04-23 12:05:02 +0200

    feat: create AvailabilityStatusDto

---
+++ src/main/java/tech/derfeb/portfolio_cms/Dto/AvailabilityStatusDto.java
@@ -0,0 +1,12 @@
+package tech.derfeb.portfolio_cms.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailabilityStatusDto {
+    private Boolean availableForWork;
+}
